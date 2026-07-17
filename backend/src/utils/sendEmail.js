67d94109d0947c9ddf2b6eb489434772e5ab646b
devannshi-24 <devannshii24@gmail.com>
2026-07-18 01:09:09 +0530

    replaced smtp with brevo api key

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,31 +1,34 @@
-import nodemailer from "nodemailer"
-
-
-
 export const sendOTPEmail = async (email, otp) => {
+  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": process.env.BREVO_API_KEY,
+    },
+    body: JSON.stringify({
+      sender: {
+        name: "MedSync",
+        email: process.env.EMAIL_FROM,
+      },
+      to: [
+        {
+          email,
+        },
+      ],
+      subject: "MedSync Email Verification",
+      htmlContent: `
+        <h2>Email Verification</h2>
+        <p>Your OTP is:</p>
+        <h1>${otp}</h1>
+        <p>This OTP will expire in 5 minutes.</p>
+      `,
+    }),
+  });
 
-  
+  if (!response.ok) {
+    const error = await response.text();
+    throw new Error(error);
+  }
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: "MedSync Email Verification",
-        html: `
-            <h2>Email Verification</h2>
-            <p>Your OTP is:</p>
-            <h1>${otp}</h1>
-            <p>This OTP will expire in 5 minutes.</p>
-        `
-    });
+  return await response.json();
 };
-
