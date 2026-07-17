34310b13a5dab19e885a55dd9f9db1aad92f2d9a
devannshi-24 <devannshii24@gmail.com>
2026-07-18 00:25:13 +0530

    otp bug fixing

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -6,14 +6,18 @@ export const sendOTPEmail = async (email, otp) => {
 
   
 
-    const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
-     }
-    })
-
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
 
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
