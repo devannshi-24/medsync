8b017a78e8ad01f851e0622e6844ff79e6f9efcc
devannshi-24 <devannshii24@gmail.com>
2026-07-18 02:29:15 +0530

    origin change

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js"
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js",
 );
 
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js",
 );
 
 firebase.initializeApp({
@@ -12,36 +12,28 @@ firebase.initializeApp({
   projectId: "medsync-2204a",
   storageBucket: "medsync-2204a.firebasestorage.app",
   messagingSenderId: "294936359546",
-  appId: "1:294936359546:web:3b5b9e704f8fa6e7fcc1c3"
+  appId: "1:294936359546:web:3b5b9e704f8fa6e7fcc1c3",
 });
 
 const messaging = firebase.messaging();
 
 // Handles notifications when the app is in background
 messaging.onBackgroundMessage((payload) => {
-    console.log("Received background message:", payload);
+  console.log("Received background message:", payload);
 
-    self.registration.showNotification(
-        payload.notification.title,
-        {
-            body: payload.notification.body,
-            icon: "/logo.png",      // optional
-            data: payload.data      // we'll use this later
-        }
-    );
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/logo.png", // optional
+    data: payload.data, // we'll use this later
+  });
 });
 
 self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
-    const scheduleId = event.notification.data?.scheduleId;
-    let url = "http://localhost:5173/dashboard";
+  event.notification.close();
+  const scheduleId = event.notification.data?.scheduleId;
+  const url = scheduleId ? `/dashboard?scheduleId=${scheduleId}` : "/dashboard";
 
-    if (scheduleId) {
-        url = `http://localhost:5173/dashboard?scheduleId=${scheduleId}`;
-    }
-    event.waitUntil(
-        clients.openWindow(url)
-    );
+  event.waitUntil(clients.openWindow(url));
 });
 
 firebase.messaging();
